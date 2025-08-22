30797b6c8b5176e2aa62e8f1dc35622474e1b588
Tiago <t.pescara@itecriocuarto.org.ar>
2025-08-22 10:11:33 -0300

    Subiendo valen

---
+++ src/pages/contrato.jsx
@@ -0,0 +1,14 @@
+import { CardsPrincipales } from "../components/CardsPrincipales";
+import { Header } from "../components/Header";
+import React from "react";
+import { FormContrato } from "../components/FormContrato"
+
+
+export default function Contrato() {
+  return (
+    <div>
+      <Header />
+      <FormContrato></FormContrato>
+    </div>
+  );
+}
